bdfa51008e8bb9cf4c7b7c2bb3e4311344f6e578
donrestarone <shashikejayatunge@gmail.com>
2020-07-19 08:28:15 -0700

    input validation for non whole numbers

---
+++ guess-number/components/Input/Input.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {TextInput, StyleSheet} from 'react-native'
+
+export default function Input(props) {
+  return (
+    <TextInput
+      {...props}
+      style={{...styles.input, ...props.style}}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 30,
+    borderBottomColor: 'grey',
+    borderBottomWidth: 1,
+    // equivalent to CSS margin-top/margin-bottom
+    marginVertical: 10
+  }
+})
--- guess-number/screens/StartGameScreen/StartGameScreen.js
+++ guess-number/screens/StartGameScreen/StartGameScreen.js
@@ -1,15 +1,33 @@
-import React from 'react'
-import {StyleSheet, Text, View, TextInput, Button} from 'react-native'
+import React, {useState} from 'react'
+import {StyleSheet, Text, View, Button} from 'react-native'
 import Card from '../../components/Card/Card'
 import Colors from '../../constants/Colors'
+import Input from '../../components/Input/Input'
 
 export default function StartGameScreen(props) {
+  let [enteredNum, setEnteredNum] = useState('')
+  let regexForWholeNumbers = /[^0-9]/g
+  function numberInputHandler(inputText) {
+    // replaces any non whole number with an empty string ''
+    setEnteredNum(inputText.replace(regexForWholeNumbers, ''))
+  }
+
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <TextInput keyboardType="number-pad" placeholder="Type your guess"/>
+        <Input 
+          value={enteredNum}
+          keyboardType="number-pad" 
+          placeholder="Guess"
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize="none"
+          autoCorrect={false}
+          maxLength={2}
+          onChangeText={numberInputHandler}
+        />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
             <Button title="Reset" color={Colors.accent} onPress={() => {}}/>
@@ -38,6 +56,10 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: '80%',
     alignItems: 'center',
+  },
+  input: {
+    width: 50,
+    textAlign: 'center',
   },
   buttonContainer: {
     flexDirection: 'row',
